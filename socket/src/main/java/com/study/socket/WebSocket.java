b39b1f89f9a40bca9b64f8f0e820a6b9b10427b5
95degree <95degree@naver.com>
2021-07-18 22:28:35 +0900

    webSocket 따라해보기

--- socket/src/main/java/com/study/socket/WebSocket.java
+++ socket/src/main/java/com/study/socket/WebSocket.java
@@ -1,14 +1,18 @@
 package com.study.socket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 @ServerEndpoint("/websocket")
 public class WebSocket{
 
@@ -19,6 +23,12 @@ public class WebSocket{
         if(session != null){
             String sessionId = session.getId();
             sessionList.add(session);
+            log.info(sessionId);
         }
+    }
+
+    @OnMessage
+    public void handleMessage(String string, Session session) throws IOException {
+        session.getBasicRemote().sendText(string+"안녕하세요");
     }
 }
